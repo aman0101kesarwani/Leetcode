095d374a118ba5e1cd0a8ceb9af2e9c9aeb0bf83
aman0101kesarwani <142049814+aman0101kesarwani@users.noreply.github.com>
2026-02-24 14:09:57 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (86.87%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -3,14 +3,6 @@ class Solution {
 
         if (s.length() != goal.length()) return false;
 
-        int n = s.length();
-
-        for (int idx = 0; idx < n; idx++) {
-
-            if (s.equals(goal.substring(idx) + goal.substring(0, idx)))
-                return true;
-        }
-
-        return false;
+        return (s+s).contains(goal);
     }
 }
