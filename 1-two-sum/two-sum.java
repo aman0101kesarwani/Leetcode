e906c3f8398ae13ef480f0bbd02950edb8d79014
aman0101kesarwani <142049814+aman0101kesarwani@users.noreply.github.com>
2026-03-25 00:34:14 +0530

    Time: 2 ms (99.26%) | Memory: 47.3 MB (33.46%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -3,17 +3,15 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
 
         for(int i=0; i<nums.length; i++){
-            map.put(nums[i], i);    // element : index
-        }
-
-        for(int i=0; i<nums.length; i++){
             int lookupNumber = target - nums[i];
 
-            if(map.containsKey(lookupNumber) && map.get(lookupNumber)!=i){
-                return new int[] {map.get(lookupNumber),i};
+            if(map.containsKey(lookupNumber)){
+                return new int[] {map.get(lookupNumber), i};
             }
-
+            map.put(nums[i], i);
         }
+
+        
 
         return new int[] {-1, -1};
     }
