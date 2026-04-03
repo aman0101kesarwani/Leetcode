e4a8bcb7c7408f7490a58a2e1883ef990a256afb
aman0101kesarwani <142049814+aman0101kesarwani@users.noreply.github.com>
2026-04-03 17:35:07 +0530

    Time: 6 ms (98.37%) | Memory: 48.1 MB (99.43%) - LeetSync

--- 561-array-partition/array-partition.java
+++ 561-array-partition/array-partition.java
@@ -1,10 +1,31 @@
 class Solution {
     public int arrayPairSum(int[] nums) {
-        Arrays.sort(nums);
+        //as constraints is not very big,
+
+        // values :(negative)..........0.............(positive)
+        // index :   0...............10^4............2x10^4 ====> total index = 2 * 10^4
+
+        int offset = 10000;
+        int[] count = new int[2 * 10000 + 1];
+
+        // store frequency
+        for (int num : nums) {
+            count[num + offset]++;
+        }
 
         int sum = 0;
-        for(int i=0; i<nums.length; i=i+2){
-            sum += nums[i];
+        boolean take = true; // simulate even index
+
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+
+                if (take) {
+                    sum += (i - offset);
+                }
+
+                take = !take; // alternate
+                count[i]--;
+            }
         }
 
         return sum;
