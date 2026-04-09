7fed2f8782a6bad2cccb3643b800071b37ee6a7a
aman0101kesarwani <142049814+aman0101kesarwani@users.noreply.github.com>
2026-04-10 01:16:19 +0530

    Time: 2 ms (99.60%) | Memory: 44.9 MB (52.45%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,16 +1,16 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        //m1: using freq array
+        //m2: using HashMap
 
-        if(s.length() != t.length()) return false;
-        
         int freq[] = new int[26];
 
-        for(char ch : s.toCharArray()){
-            freq[ch - 'a']++;
+        for(char c: s.toCharArray()){
+            freq[c - 'a']++;
         }
 
-        for(char ch : t.toCharArray()){
-            freq[ch - 'a']--;
+        for(char c : t.toCharArray()){
+            freq[c - 'a']--;
         }
 
         for(int i=0; i<freq.length; i++){
@@ -18,6 +18,5 @@ class Solution {
         }
 
         return true;
-        
     }
 }
