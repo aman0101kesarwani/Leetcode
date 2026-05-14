b1af919bffd3cec063e7df0aeab0c5f9d892c7be
aman0101kesarwani <142049814+aman0101kesarwani@users.noreply.github.com>
2026-05-14 23:22:54 +0530

    Time: 3 ms (59.73%) | Memory: 44.8 MB (44.44%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+
+            if(c>='A' && c<='Z'){
+                char lower = (char)(c+32);
+                sb.append(lower);
+            }
+            else if(c>='a' && c<='z'){
+                sb.append(c);
+            }
+            else if(c >= '0' && c <= '9'){
+                sb.append(c);
+            }
+        }
+
+        int left = 0;
+        int right=sb.length()-1;
+
+        while(left<=right){
+            if(sb.charAt(left) != sb.charAt(right)){
+                return false;
+            }
+            else{
+                left++;
+                right--;
+            }
+            
+        }
+
+        return true;
+    }
+}
