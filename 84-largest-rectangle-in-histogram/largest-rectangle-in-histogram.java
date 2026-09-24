313a49b692f572fa463c63765ab850a926312c81
aman0101kesarwani <142049814+aman0101kesarwani@users.noreply.github.com>
2026-09-24 14:00:09 +0530

    Time: 92 ms (33.02%) | Memory: 76 MB (94.18%) - LeetSync

---
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int n= heights.length;
+        if(n==1) return heights[0];
+        Stack<Integer> ps = new Stack<>();    //enter index
+        int[] pse = new int[n];
+        pse[0]=-1;
+        ps.push(0);
+        for(int i=1; i<n; i++){
+            while(ps.size()>0 && heights[ps.peek()]>=heights[i]) ps.pop();
+            if(ps.size()==0) pse[i]=-1;
+            else pse[i]=ps.peek();       //for this we need to store index in stack
+
+            ps.push(i);
+        }
+
+        Stack<Integer> ns = new Stack<>();
+        int nse[] = new int[n];
+        ns.push(n-1);
+        nse[n - 1] = n;
+        for(int i=n-2; i>=0; i--){
+            while(ns.size()>0 && heights[ns.peek()]>=heights[i]) ns.pop();
+            if(ns.size()==0) nse[i]=n;
+            else nse[i]=ns.peek();    // that's why we stored index in stack
+
+            ns.push(i);
+        }
+
+        int max=0;
+        for(int i=0; i<n; i++){
+            max = Math.max(heights[i]*(nse[i]-pse[i]-1), max);      //max area
+        }
+
+        return max;
+    }
+}
